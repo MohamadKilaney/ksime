dd7e4bf2d48319976a05009eccff67730704fdda
Mohamad Kilaney <80094486+MohamadKilaney@users.noreply.github.com>
2021-04-29 00:18:54 +0300

    add cat images

--- client/src/components/category/category.js
+++ client/src/components/category/category.js
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 import "./category.css";
 
 //import images
-import img1 from "../../images/category/category chair.png"
-import img2 from "../../images/category/category closet.png"
-import img3 from "../../images/category/category office.png"
+import img1 from "../../images/category/category chairs.png"
+import img2 from "../../images/category/category closet (1).png"
+import img3 from "../../images/category/category community.png"
+import img4 from "../../images/category/category desk.png"
+import img5 from "../../images/category/category office (1).png"
+import img6 from "../../images/category/category recieve.png"
 
 
 function Category() {
@@ -13,9 +16,9 @@ function Category() {
             <div className="cat"><img src={img1} /></div>
             <div className="cat"><img src={img2} /></div>
             <div className="cat"><img src={img3} /></div>
-            <div className="cat"><img src={img1} /></div>
-            <div className="cat"><img src={img2} /></div>
-            <div className="cat"><img src={img3} /></div>
+            <div className="cat"><img src={img4} /></div>
+            <div className="cat"><img src={img5} /></div>
+            <div className="cat"><img src={img6} /></div>
         </div>
     );
 };
