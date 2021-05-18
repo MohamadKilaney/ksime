94a8fc73594c173781a5246d377afca0900003c2
Mohamad Kilaney <80094486+MohamadKilaney@users.noreply.github.com>
2021-05-18 16:04:51 +0300

    update

--- client/src/pages/products/product.js
+++ client/src/pages/products/product.js
@@ -5,7 +5,12 @@ import Header from '../../components/header/header';
 import Navbar from '../../components/navbar/navbar';
 import Card from '../../components/cards/card';
 import Category from '../../components/category/category'
+import Pic1 from '../../images/francesca-tosolini-Mx_W2atEaDo-unsplash.png'
+import { Parallax } from 'react-parallax';
 
+
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 
 import {
   BrowserRouter as Router,
@@ -16,11 +21,24 @@ import {
 
 
 function Products() {
+  const options = [
+    'lowest to highest', 'highest to lowest'
+  ];
+  const defaultOption = options[0];
   return (
     <div className="Products">
       <Header />
-      <Navbar />
-      
+
+      <Parallax bgImage={Pic1} strength={500}>
+        <div className="pic-pic1">
+          <span className="title">chiars</span>
+        </div>
+      </Parallax>
+      <div className="sortType">
+        Sort by    <Dropdown options={options} placeholder="popularity" className="test" />
+      </div>
+
+
       <div className="container">
         <Card />
         <Card />
@@ -30,8 +48,10 @@ function Products() {
         <Card />
         <Card />
         <Card />
+        <Card />
+        <Card />
       </div>
-      
+
     </div >
   );
 }
